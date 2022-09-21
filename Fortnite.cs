835b52f4dae59172efa9a85cd90bbbc8330e4a92
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-21 14:25:16 +0300

    Add files via upload

---
+++ Fortnite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.DirectoryServices.ActiveDirectory;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AuroraLauncher
+{
+    class Fortnite
+    {
+        Process process3 = System.Diagnostics.Process.Start("PathTextBox.Text" + "\\FortniteGame\\Binaries\\Win64\\FortniteClient-Win64-Shipping.exe");
+    }
+}
